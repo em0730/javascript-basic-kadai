d46fb50e7fbdb17c3f74899e8fd7439b7fa3522d
em0730 <eri21jan3904@gmail.com>
2024-10-06 12:08:41 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,8 +1,6 @@
-let bodyWeight;
-bodyWeigh = 68;
+let bodyWeight=68;
 let height;
 height =1.7;
 let bmi;
-bmi =(bodyWeight/(height*height));
-
-console.log(68/(1.7*1.7));
+bmi = bodyWeight/(height*height);
+console.log(bmi);
